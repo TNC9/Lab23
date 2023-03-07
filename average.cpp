8c8e28bf5fbfb73bee6375eee0837212bb0de287
TNC9 <koonbenzkub@hotmail.com>
2023-03-07 13:04:48 +0700

    Complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char const *argv[])
+{
+    if (argc == 1)
+    {
+        cout << "Please input numbers to find average.";
+    }
+    else{
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << argc-1 << " numbers = " ;
+        double Average = 0;
+        for (int i = 0; i < argc; i++)
+        {
+            Average += atof(argv[i])/(argc-1);
+        }
+        cout << Average << endl;
+        cout << "---------------------------------" ;
+        
+    }
+    return 0;
+}
